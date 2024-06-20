ff1de1f41db97f82ee47dcf4996f71be2da8c0e6
Kavyareddivari9 <150787884+Kavyareddivari9@users.noreply.github.com>
2024-06-20 18:40:05 +0530

    Update main.tf

--- privatecluster/main.tf
+++ privatecluster/main.tf
@@ -191,3 +191,14 @@ resource "random_string" "suffix" {
   special = false
   upper   = false
 }
+resource "google_compute_firewall" "allow_http" {
+  name    = "allow-http-rule"
+  network = "default"
+  allow {
+    ports    = ["80"]
+    protocol = "tcp"
+  }
+  target_tags = ["allow-http"]
+  priority    = 1000
+
+}
