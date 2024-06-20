930793306edc6e8015ff8e3c05330263f05a514e
Kavyareddivari9 <150787884+Kavyareddivari9@users.noreply.github.com>
2024-06-20 10:34:21 +0530

    Create main.tf

---
+++ privatecluster/main.tf
@@ -0,0 +1,3 @@
+# DEPLOY A GKE PRIVATE CLUSTER IN GOOGLE CLOUD PLATFORM
+# This is an example of how to use the gke-cluster module to deploy a private Kubernetes cluster in GCP
+# ---------------------------------------------------------------------------------------------------------------------
