f2242c918e03eb4d7e4b47a0d6fc38e323f5dcb3
Kavyareddivari9 <150787884+Kavyareddivari9@users.noreply.github.com>
2024-06-24 11:38:59 +0530

    Update main.tf

--- modules/vpc-network/main.tf
+++ modules/vpc-network/main.tf
@@ -31,68 +31,14 @@ resource "google_compute_router" "vpc_router" {
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
-# Public Subnetwork Config
-# Public internet access for instances with addresses is automatically configured by the default gateway for 0.0.0.0/0
-# External access is configured with Cloud NAT, which subsumes egress traffic for instances without external addresses
-# ---------------------------------------------------------------------------------------------------------------------
 
-resource "google_compute_subnetwork" "vpc_subnetwork_public" {
-  name = "${var.name_prefix}-subnetwork-public"
 
-  project = var.project
-  region  = var.region
-  network = google_compute_network.vpc.self_link
+  
 
-  private_ip_google_access = true
-  ip_cidr_range            = cidrsubnet(var.cidr_block, var.cidr_subnetwork_width_delta, 0)
+  
 
-  secondary_ip_range {
-    range_name = var.public_subnetwork_secondary_range_name
-    ip_cidr_range = cidrsubnet(
-      var.secondary_cidr_block,
-      var.secondary_cidr_subnetwork_width_delta,
-      0
-    )
-  }
 
-  secondary_ip_range {
-    range_name = var.public_services_secondary_range_name
-    ip_cidr_range = var.public_services_secondary_cidr_block != null ? var.public_services_secondary_cidr_block : cidrsubnet(
-      var.secondary_cidr_block,
-      var.secondary_cidr_subnetwork_width_delta,
-      1 * (2 + var.secondary_cidr_subnetwork_spacing)
-    )
-  }
-
-  dynamic "log_config" {
-    for_each = var.log_config == null ? [] : tolist([var.log_config])
-
-    content {
-      aggregation_interval = var.log_config.aggregation_interval
-      flow_sampling        = var.log_config.flow_sampling
-      metadata             = var.log_config.metadata
-    }
-  }
-}
-
-resource "google_compute_router_nat" "vpc_nat" {
-  name = "${var.name_prefix}-nat"
-
-  project = var.project
-  region  = var.region
-  router  = google_compute_router.vpc_router.name
-
-  nat_ip_allocate_option = "AUTO_ONLY"
-
-  # "Manually" define the subnetworks for which the NAT is used, so that we can exclude the public subnetwork
-  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
-
-  subnetwork {
-    name                    = google_compute_subnetwork.vpc_subnetwork_public.self_link
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
-  }
-}
-
+    
 # ---------------------------------------------------------------------------------------------------------------------
 # Private Subnetwork Config
 # ---------------------------------------------------------------------------------------------------------------------
@@ -142,8 +88,8 @@ module "network_firewall" {
 
   project                               = var.project
   network                               = google_compute_network.vpc.self_link
-  allowed_public_restricted_subnetworks = var.allowed_public_restricted_subnetworks
+  allowed_private_restricted_subnetworks = var.allowed_private_restricted_subnetworks
 
-  public_subnetwork  = google_compute_subnetwork.vpc_subnetwork_public.self_link
+  private_subnetwork  = google_compute_subnetwork.vpc_subnetwork_private.self_link
   private_subnetwork = google_compute_subnetwork.vpc_subnetwork_private.self_link
 }
