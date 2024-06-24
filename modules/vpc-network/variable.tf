c94797e86a35d203a09b45b7399da829378fe54f
Kavyareddivari9 <150787884+Kavyareddivari9@users.noreply.github.com>
2024-06-24 17:08:37 +0530

    Update variable.tf

--- modules/vpc-network/variable.tf
+++ modules/vpc-network/variable.tf
@@ -41,16 +41,16 @@ variable "cidr_subnetwork_spacing" {
   default     = 0
 }
 
-variable "public_subnetwork_secondary_range_name" {
+variable "private_subnetwork_secondary_range_name" {
   description = "The name associated with the pod subnetwork secondary range, used when adding an alias IP range to a VM instance. The name must be 1-63 characters long, and comply with RFC1035. The name must be unique within the subnetwork."
   type        = string
-  default     = "public-cluster"
+  default     = "private-cluster"
 }
 
-variable "public_services_secondary_range_name" {
+variable "private_services_secondary_range_name" {
   description = "The name associated with the services subnetwork secondary range, used when adding an alias IP range to a VM instance. The name must be 1-63 characters long, and comply with RFC1035. The name must be unique within the subnetwork."
   type        = string
-  default     = "public-services"
+  default     = "private-services"
 }
 
 variable "secondary_cidr_block" {
@@ -59,8 +59,8 @@ variable "secondary_cidr_block" {
   default     = "10.1.0.0/16"
 }
 
-variable "public_services_secondary_cidr_block" {
-  description = "The IP address range of the VPC's public services secondary address range in CIDR notation. A prefix of /16 is recommended. Do not use a prefix higher than /27. Note: this variable is optional and is used primarily for backwards compatibility, if not specified a range will be calculated using var.secondary_cidr_block, var.secondary_cidr_subnetwork_width_delta and var.secondary_cidr_subnetwork_spacing."
+variable "private_services_secondary_cidr_block" {
+  description = "The IP address range of the VPC's private services secondary address range in CIDR notation. A prefix of /16 is recommended. Do not use a prefix higher than /27. Note: this variable is optional and is used primarily for backwards compatibility, if not specified a range will be calculated using var.secondary_cidr_block, var.secondary_cidr_subnetwork_width_delta and var.secondary_cidr_subnetwork_spacing."
   type        = string
   default     = null
 }
@@ -98,8 +98,8 @@ variable "log_config" {
   }
 }
 
-variable "allowed_public_restricted_subnetworks" {
-  description = "The public networks that is allowed access to the public_restricted subnetwork of the network"
+variable "allowed_private_restricted_subnetworks" {
+  description = "The private networks that is allowed access to the private_restricted subnetwork of the network"
   default     = []
   type        = list(string)
 }
