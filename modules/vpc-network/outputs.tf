944568497e3a9bec296a17cac68dec43bf0d3435
Kavyareddivari9 <150787884+Kavyareddivari9@users.noreply.github.com>
2024-06-24 11:36:46 +0530

    Update outputs.tf

--- modules/vpc-network/outputs.tf
+++ modules/vpc-network/outputs.tf
@@ -3,45 +3,7 @@ output "network" {
   value       = google_compute_network.vpc.self_link
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
-# Public Subnetwork Outputs
-# ---------------------------------------------------------------------------------------------------------------------
-
-output "public_subnetwork" {
-  description = "A reference (self_link) to the public subnetwork"
-  value       = google_compute_subnetwork.vpc_subnetwork_public.self_link
-}
-
-output "public_subnetwork_name" {
-  description = "Name of the public subnetwork"
-  value       = google_compute_subnetwork.vpc_subnetwork_public.name
-}
-
-output "public_subnetwork_cidr_block" {
-  value = google_compute_subnetwork.vpc_subnetwork_public.ip_cidr_range
-}
-
-output "public_subnetwork_gateway" {
-  value = google_compute_subnetwork.vpc_subnetwork_public.gateway_address
-}
-
-output "public_subnetwork_secondary_cidr_block" {
-  value = google_compute_subnetwork.vpc_subnetwork_public.secondary_ip_range[0].ip_cidr_range
-}
-
-output "public_subnetwork_secondary_range_name" {
-  value = google_compute_subnetwork.vpc_subnetwork_public.secondary_ip_range[0].range_name
-}
-
-output "public_services_secondary_cidr_block" {
-  value = google_compute_subnetwork.vpc_subnetwork_public.secondary_ip_range[1].ip_cidr_range
-}
-
-output "public_services_secondary_range_name" {
-  value = google_compute_subnetwork.vpc_subnetwork_public.secondary_ip_range[1].range_name
-}
-
-# ---------------------------------------------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------------------------------------
 # Private Subnetwork Outputs
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -75,15 +37,7 @@ output "private_subnetwork_secondary_range_name" {
 # Access Tier - Network Tags
 # ---------------------------------------------------------------------------------------------------------------------
 
-output "public" {
-  description = "The network tag string used for the public access tier"
-  value       = module.network_firewall.public
-}
 
-output "public_restricted" {
-  description = "The string of the public tag"
-  value       = module.network_firewall.public_restricted
-}
 
 output "private" {
   description = "The network tag string used for the private access tier"
